7e3ad21edd2851a959ac812be28e783e23df9bd0
Felipe Barros <felipe.b4rros@gmail.com>
2020-05-01 01:46:16 -0300

    change form widget os agregar occurencia

--- SysIMiBio/imibio/forms.py
+++ SysIMiBio/imibio/forms.py
@@ -2,6 +2,25 @@ from django import forms
 from .models import Occurrences_imibio
 
 class OccForm(forms.ModelForm):
+    occurrenceID = forms.CharField(required=False)
+    basisOfRecord = forms.CharField(required=False)
+    catalogNumber = forms.CharField(required=False)
+    scientificName = forms.CharField(required=False)
+    kingdom = forms.CharField(required=False)
+    phylum = forms.CharField(required=False)
+    clase = forms.CharField(required=False)
+    order = forms.CharField(required=False)
+    family = forms.CharField(required=False)
+    genus = forms.CharField(required=False)
+    specificEpithet = forms.CharField(required=False)
+    taxonRank = forms.CharField(required=False)
+    infraspecificEpithet = forms.CharField(required=False)
+    identificationQualifier = forms.CharField(required=False)
+    county = forms.CharField(required=False)
+    stateProvince = forms.CharField(required=False)
+    locality = forms.CharField(required=False)
+    recordedBy = forms.CharField(required=False)
+    recordNumber = forms.CharField(required=False)
 
     class Meta:
         model = Occurrences_imibio
