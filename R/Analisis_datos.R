2d33a7ac0287383f15f978e16751a2d785594aad
Felipe Barros <felipe.b4rros@gmail.com>
2020-05-13 22:49:13 -0300

    update query to a more general approach

--- R/Analisis_datos.R
+++ R/Analisis_datos.R
@@ -1,9 +1,11 @@
 # load libraries ----
+library(data.table)
 library(dbplyr)
 library(dplyr)
 library(RPostgres)
 library(DBI)
 library(ggplot2)
+library(readr)
 
 # Create a connection to the database ----
 # Connect to a specific postgres database i.e. Heroku
@@ -78,100 +80,137 @@ occs %>%
   summarise(spp_total = sum(spp_total, na.rm=TRUE)) %>% collect()
 
 # Grupos para Argentina ----
-occs %>%
-  group_by(kingdom, phylum, clase, order, family) %>% 
+gruposArgentina <- occs %>%
+  filter(
+    taxonRank == 'SPECIES',
+    taxonomicStatus == 'ACCEPTED',
+    kingdom %in% c(
+      'Fungi', "Protozoa", "Bacteria", "Plantae", "Animalia") |
+      phylum %in% c(
+        "Arthropoda", "Mollusca") |
+      clase %in% c(
+        "Aves", "Mammalia", "Amphibia", "Reptilia", 
+        "Actinopterygii", "Elasmobranchii", "Insecta", "Arachnida", "Malacostraca")) %>% 
+  group_by(kingdom, phylum, clase) %>% 
   summarise(spp_total = count(distinct(species))) %>%
   arrange(desc(spp_total)) %>% 
   ungroup() %>% 
-  filter(
-    kingdom %in% c('Fungi', "Protozoa", "Bacteria") |
-      phylum == "Mollusca" |
-      clase %in% c(
-        "Mammalia", "Aves", "Amphibia", "Reptilia",
-        "Myxini", "Hyperoartia", "Chondrichthyes", "Actinopterygii", "Sarcopterygii", # peces
-        "Rodophyta", "Chlorophyta", "Phaeophyceae", "Chrysophyceae", "Euglenoidea", "Ellobiopsea", "Dinophyceae", # algas
-        
-        "Magnoliopsida", "Insecta", "Arachnida",
-                       "Rodophyta", "Chlorophyta", "Phaeophyceae", "Chrysophyceae", 
-                       "Euglenoidea", "Ellobiopsea", "Dinophyceae",
-                   "Cycadopsida", "Ginkgoopsida", "Pinopsida", "Gnetopsida") |
-      order %in% c("Coleoptera", "Lepidoptera", "Decapoda", "Diptera", 
-                   "Cycadales", "Ginkgoales", "Pinales", "Gnatales", # plantas
-                   "Amborellales", "Nymphaeales", "Austrobaileyales",
-                   'Chloranthales', "Magnoliales", "Laurales", "Canellales",
-                   "Piperales", "Acorales", "Alismatales", "Petrosaviales",
-                   "Dioscoreales", "Pandanales", "Liliales", "Asparagales",
-                   "Arecales", "Poales", "Commelinales", "Zingiberales",
-                   "Ceratophyllales", "Ranunculales", "Sabiaceae",
-                   "Proteales", "Trochodendrales", "Buxales",
-                   "Gunnerales", "Dillniaceae", "Saxifragales",
-                   "Vitales", "Zygophyllales", "Celatrales",
-                   "Oxalidades", "Malpighiales", "Fabales",
-                   "Rosales", "Cucurbitales", "Fagales",
-                   "Geraniales", "Myrtales", "Crossosomatales",
-                   "Picramniales", "Sapindales", "Huerteales",
-                   "Malvales", "Brassicales", "Santalales",
-                   "Berberidopsiadales", "Caryopphyllales",
-                   "Cornales", "Ericales", "Garryales", "Gentianales",
-                   "Solanales", "Boraginales", "Lamiales",
-                   "Aquifoliales", "Asterales", "Escalloniales",
-                   "Bruniales", "Apiales", 
-                   "Paracryphiales", "Dipsacales" # plantas
-                   ) |
-      family %in% c("Formicidae", "Apidae") # hormigas y abejas
-    ) %>% 
-  mutate(
-    grupo = dplyr::case_when(
-      kingdom == 'Fungi' ~'Hongos',
-      kingdom %in% c("Bacteria", 'Protozoa') ~'Protozoos y Bacterias',
-      
-      phylum == "Mollusca" ~"Moluscos",
-      
-      clase == "Mammalia" ~ "Mamiferos",
-      clase == "Aves" ~ "Aves",
-      clase == "Amphibia" ~"Amphibia",
-      clase == "Reptilia" ~"Reptilia",
-      clase %in% c("Myxini", "Hyperoartia", "Chondrichthyes", 
-                   "Actinopterygii", "Sarcopterygii") ~ 'Peces',
-      clase %in% c("Rodophyta", "Chlorophyta", "Phaeophyceae", 
-                   "Chrysophyceae", "Euglenoidea", 
-                   "Ellobiopsea", "Dinophyceae") ~ 'Algas (dulceacuícolas y terrestres)',
-      #clase %in% c("Cycadopsida", "Ginkgoopsida", "Pinopsida", "Gnetopsida") ~'Gimnospermas',
-      
-      order == "Coleoptera"  ~ 'Escarabajos',
-      order == "Lepidoptera" ~ 'Mariposas',
-      order == 'Decapoda' ~ 'Decápodos (dulceacuícolas y terrestres)',
-      order == "Diptera" ~ 'Dípteros',
-      order %in% c("Cycadales", "Ginkgoales", "Pinales", "Gnatales",
-                   "Amborellales", "Nymphaeales", "Austrobaileyales",
-                   'Chloranthales', "Magnoliales", "Laurales", "Canellales",
-                   "Piperales", "Acorales", "Alismatales", "Petrosaviales",
-                   "Dioscoreales", "Pandanales", "Liliales", "Asparagales",
-                   "Arecales", "Poales", "Commelinales", "Zingiberales",
-                   "Ceratophyllales", "Ranunculales", "Sabiaceae",
-                   "Proteales", "Trochodendrales", "Buxales",
-                   "Gunnerales", "Dillniaceae", "Saxifragales",
-                   "Vitales", "Zygophyllales", "Celatrales",
-                   "Oxalidades", "Malpighiales", "Fabales",
-                   "Rosales", "Cucurbitales", "Fagales",
-                   "Geraniales", "Myrtales", "Crossosomatales",
-                   "Picramniales", "Sapindales", "Huerteales",
-                   "Malvales", "Brassicales", "Santalales",
-                   "Berberidopsiadales", "Caryopphyllales",
-                   "Cornales", "Ericales", "Garryales", "Gentianales",
-                   "Solanales", "Boraginales", "Lamiales",
-                   "Aquifoliales", "Asterales", "Escalloniales",
-                   "Bruniales", "Apiales", 
-                   "Paracryphiales", "Dipsacales") ~'Angiospermas',
-
-      family == "Formicidae" ~'Hormigas',
-      family == "Apidae" ~'Abejas',
-      #family == 'Dasypogonaceae' ~'Angiospermas',
-      
-      TRUE ~ order)) %>% 
-  group_by(grupo) %>% 
-  summarise(spp_total = sum(spp_total, na.rm=TRUE)) %>%
-  arrange(desc(spp_total)) %>% collect() %>% 
-  write.csv("./output/GruposArgentina.csv")
+      mutate(
+        grupo = dplyr::case_when(
+          
+          # peces
+          (kingdom == "Animalia" & clase %in%
+             c('Actinopterygii', 'Elasmobranchii')) ~'Reino Animalia y Clases Peces', # reino Animalia Y clases definidas
+          
+          # Aves
+          (kingdom == "Animalia" & clase == "Aves") ~ 'Reino Animalia y Order Aves',
+          
+          (kingdom == "Animalia" & clase == "Mammalia") ~ 'Reino Animalia y Clase Mammalia',
+          
+          (kingdom == "Animalia" & clase == "Amphibia") ~ 'Reino Animalia y Order Amphibia',
+          
+          (kingdom == "Animalia" & clase == "Reptilia") ~ 'Reino Animalia y Order Reptilia',
+          
+          (kingdom == "Animalia" & phylum == "Arthropoda") ~ 'Reino Animalia y phylum Arthropoda',
+          (kingdom == "Animalia" & phylum == "Arthropoda" &
+             clase == 'Insecta') ~ 'Reino Animalia y phylum Arthropoda y clase Insecta',
+          (kingdom == "Animalia" & phylum == "Arthropoda" &
+             clase == 'Arachnida') ~ 'Reino Animalia y phylum Arthropoda y clase Arachnida',
+          (kingdom == "Animalia" & phylum == "Arthropoda" &
+             clase == 'Malacostraca') ~ 'Reino Animalia y phylum Arthropoda y clase Malacostraca',
+          
+          (kingdom == "Animalia" & phylum == "Mollusca") ~ 'Reino Animalia y phylum Mollusca',
+          
+          kingdom == "Plantae" ~"Plantas",
+          kingdom == "Animalia" ~"Animales",
+          kingdom == 'Fungi' ~'Hongos',
+          kingdom == "Bacteria" ~'Bacterias',
+          kingdom == 'Protozoa' ~'Protozoos'#,
+          
+          #TRUE ~ "No cumplió ninguna regla"
+          )) %>% 
+      group_by(grupo) %>% 
+      summarise(spp_total = sum(spp_total, na.rm=TRUE)) %>%
+      arrange(desc(spp_total)) %>% collect()
+#gruposArgentina %>% 
+ # write_csv("./output/GruposArgentina.csv")
 
 # Grupos para Misiones Txt ----
+gruposMisiones <- occs %>%
+  filter(
+    taxonRank == 'SPECIES',
+    taxonomicStatus == 'ACCEPTED',
+    stateProvince %like% '%siones',
+    kingdom %in% c(
+      'Fungi', "Protozoa", "Bacteria", "Plantae", "Animalia") |
+      phylum %in% c(
+        "Arthropoda", "Mollusca") |
+      clase %in% c(
+        "Aves", "Mammalia", "Amphibia", "Reptilia", 
+        "Actinopterygii", "Elasmobranchii", "Insecta", "Arachnida", "Malacostraca")) %>% 
+  group_by(kingdom, phylum, clase) %>% 
+  summarise(spp_total = count(distinct(species))) %>%
+  arrange(desc(spp_total)) %>% 
+  ungroup() %>% 
+  mutate(
+    grupo = dplyr::case_when(
+      
+      # peces
+      (kingdom == "Animalia" & clase %in%
+         c('Actinopterygii', 'Elasmobranchii')) ~'Reino Animalia y Clases Peces', # reino Animalia Y clases definidas
+      
+      # Aves
+      (kingdom == "Animalia" & clase == "Aves") ~ 'Reino Animalia y Order Aves',
+      
+      (kingdom == "Animalia" & clase == "Mammalia") ~ 'Reino Animalia y Clase Mammalia',
+      
+      (kingdom == "Animalia" & clase == "Amphibia") ~ 'Reino Animalia y Order Amphibia',
+      
+      (kingdom == "Animalia" & clase == "Reptilia") ~ 'Reino Animalia y Order Reptilia',
+      
+      (kingdom == "Animalia" & phylum == "Arthropoda") ~ 'Reino Animalia y phylum Arthropoda',
+      (kingdom == "Animalia" & phylum == "Arthropoda" &
+         clase == 'Insecta') ~ 'Reino Animalia y phylum Arthropoda y clase Insecta',
+      (kingdom == "Animalia" & phylum == "Arthropoda" &
+         clase == 'Arachnida') ~ 'Reino Animalia y phylum Arthropoda y clase Arachnida',
+      (kingdom == "Animalia" & phylum == "Arthropoda" &
+         clase == 'Malacostraca') ~ 'Reino Animalia y phylum Arthropoda y clase Malacostraca',
+      
+      (kingdom == "Animalia" & phylum == "Mollusca") ~ 'Reino Animalia y phylum Mollusca',
+      
+      kingdom == "Plantae" ~"Plantas",
+      kingdom == "Animalia" ~"Animales",
+      kingdom == 'Fungi' ~'Hongos',
+      kingdom == "Bacteria" ~'Bacterias',
+      kingdom == 'Protozoa' ~'Protozoos'#,
+      
+      #TRUE ~ "No cumplió ninguna regla"
+    )) %>% 
+  group_by(grupo) %>% 
+  summarise(spp_total = sum(spp_total, na.rm=TRUE)) %>%
+  arrange(desc(spp_total)) %>% collect()
+
+#gruposMisiones %>% 
+ # write_csv("./output/GruposMisiones.csv")
+
+# Merging data
+(gruposArgentina <- read_csv("./output/GruposArgentina.csv"))
+(gruposMisiones <- read_csv("./output/GruposMisiones.csv"))
+mergedData <- gruposArgentina %>% left_join(gruposMisiones,suffix = c("_Ar", "_Mis"), by = "grupo")
+mergedData %>% 
+  mutate(
+    "perc_Mis" = (spp_total_Mis/spp_total_Ar) * 100
+  ) %>% arrange(desc(perc_Mis)) %>% 
+  write_csv("./output/Grupos_ArgentinaMisiones.csv")
+
+# Teste sf ----
+library(sf)
+spp_sf <- st_read(
+  dsn = con,
+  layer = 'sndb_occurrences',
+  #query = "select species, geom_original from sndb_occurrences",
+  EWKB = TRUE,
+  quiet = FALSE,
+  as_tibble = FALSE,
+  geometry_column = "geom_original"
+)
