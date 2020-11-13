7e6c2bf3c4e191fb3418708aefa55517b4548d8e
Felipe Barros <felipe.b4rros@gmail.com>
2020-11-13 15:49:15 -0300

    inicio refatoracao :warning: abandonada

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn sysimibio.wisg --log-file -
--- SysIMiBio/settings.py
+++ SysIMiBio/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 from decouple import config, Csv
-from dj_database_url import parse as db_url
+from dj_database_url import parse as dburl
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -91,16 +91,20 @@ WSGI_APPLICATION = 'SysIMiBio.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, '.sqlite3')
 DATABASES = {
-    'default': {
-         'ENGINE': 'django.contrib.gis.db.backends.postgis',
-         'NAME': config('DBNAME'),
-         'USER': config('DBUSER'),
-         'PASSWORD': config('DBPASSWORD'),
-         'HOST': 'localhost',
-         'PORT': '',
-    },
+    'default': config('DATABASE_URL', default=default_dburl, cast=dburl),
 }
+# DATABASES = {
+#     'default': {
+#          'ENGINE': 'django.contrib.gis.db.backends.postgis',
+#          'NAME': config('DBNAME'),
+#          'USER': config('DBUSER'),
+#          'PASSWORD': config('DBPASSWORD'),
+#          'HOST': 'localhost',
+#          'PORT': '',
+#     },
+# }
 """
 DATABASES = {
     'default': config('DATABASE_URL', cast=db_url),
